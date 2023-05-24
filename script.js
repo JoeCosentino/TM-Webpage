f5fe46de02dac133b95714a66f6ea680d65fc6e5
Joey Cos <joeycosentino24@gmail.com>
2023-05-24 08:47:07 -0400

    menu bar releases smoothly

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const toggleButton = document.getElementById('toggle-btn');
 const navBarLinks = document.getElementById('navbar-links');
+const header = document.getElementById('header');
 
 toggleButton.addEventListener('click', () => {
     navBarLinks.classList.toggle('active');
+    header.classList.toggle('active')
 });
+
+// toggleButton.addEventListener('click')
